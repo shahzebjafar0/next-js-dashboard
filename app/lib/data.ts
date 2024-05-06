a0771c09ca61275dba67b0718c190ec7ab776c09
shahzebjafar0 <shahzebjafar006@gmail.com>
2024-05-07 04:14:39 +0500

    add DB setup and fetch data for dashboard cards

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -64,7 +64,7 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
 
-    const data = await Promise.all([
+   const data = await Promise.all([
       invoiceCountPromise,
       customerCountPromise,
       invoiceStatusPromise,
